f704c375842046525b21e94a5bb655555f7eaee8
ocawthorne <ocawthorne@gmail.com>
2021-01-20 17:17:40 +0000

    find-lab

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-const testVar = {}
-
-function testFunc() {
-  return "hi"
+function superbowlWin(array) {
+  let year
+  array.find(function(obj) {
+    if (obj.result === "W") year = obj.year
+  })
+  return year
 }
